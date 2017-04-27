57d86578c58781bd7840807b03b958a3dbb52ba7
AcrixF <acrixf@gmail.com>
2017-04-27 16:47:25 -0500

    Finalizado!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
+//Angular Core
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+//Componentes
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
+
+//Rutas
+import { APP_ROUTING } from "./app.routes";
+
+//Servicios
+import { HeroesService } from './services/heroes.service';
+
+//Pipes
+import { KeysPipe } from './pipes/keys.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent,
+    KeysPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [HeroesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/interfaces/heroe.interface.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by root on 19/04/17.
+ */
+export interface Heroe {
+  nombre: string;
+  bio: string;
+  casa: string;
+  $key?: string;
+}
